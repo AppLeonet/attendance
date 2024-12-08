9da5057843f0137363342e1b37f1dfd9580d8140
leo <hozhengmandeng@gmail.com>
2024-12-08 18:33:51 -0500

    Update Database Records

---
+++ editpost.php
@@ -0,0 +1,33 @@
+<?php 
+    //$title = 'Success';
+    //require_once 'includes/header.php'; 
+    require_once 'db/conn.php';
+
+
+//get values from post operation
+if(isset($_POST['submit'])){
+        //extract values from the $_POST array
+        $id = $_POST['id'];
+        $fname = $_POST['firstname'];
+        $lname = $_POST['lastname'];
+        $inputDate = $_POST['dob'];
+        $formattedDate = date('Y/m/d', strtotime($inputDate));
+        $dob = $formattedDate;
+        $email = $_POST['email'];
+        $contact = $_POST['phone'];
+        $specialty = $_POST['specialty'];
+
+
+//call crud function
+    $result = $crud->editAttendee($id, $fname, $lname, $dob, $email, $contact, $specialty);
+//redirect to index.php
+    if($result) {
+        header("Location: viewrecords.php");
+    } else {
+        echo 'error';
+    }
+} else {
+    echo 'error';
+}
+?>
+<?php require_once 'includes/footer.php'; ?>
--- viewrecords.php
+++ viewrecords.php
@@ -28,7 +28,10 @@
         <td><?php //echo $r['emailaddress']?></td>
         <td><?php //echo $r['contactnumber']?></td-->
         <td><?php echo $r['name']?></td>
-        <td><a href="view.php?id=<?php echo $r['attendee_id']?>" class="btn btn-primary">View</a></td>
+        <td>
+            <a href="view.php?id=<?php echo $r['attendee_id']?>" class="btn btn-primary">View</a>
+            <a href="edit.php?id=<?php echo $r['attendee_id']?>" class="btn btn-warning">Edit</a>
+        </td>
     </tr>
     <?php } ?>
 </table>
