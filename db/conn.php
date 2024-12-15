2790691c9311f3939aae35fcf7554c2d65ce84f4
leo <hozhengmandeng@gmail.com>
2024-12-14 23:52:36 -0500

    testing deploy 3

--- db/conn.php
+++ db/conn.php
@@ -22,14 +22,18 @@
 ?-->
 
 <?php
-$host = 'cat670aihdrkt1.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com';
-$db = 'd194o2jdi4gg9j';
-//$db = 'attendance_db';
-$user = 'u9rcruifdh6u3s';
-$pass = 'p8a62f1bc5e652f2eff3e00573baf1561a4324b302231d3318ec14ae98d93f4b7';
-$charset = 'utf8';
+$url = parse_url(getenv("postgres://u9rcruifdh6u3s:p8a62f1bc5e652f2eff3e00573baf1561a4324b302231d3318ec14ae98d93f4b7@cat670aihdrkt1.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d194o2jdi4gg9j"));
 
-$dsn = "pgsql:host=$host;port=5432;dbname=$db";
+$host = $url["host"];
+$port = $url["port"];
+$user = $url["user"];
+$password = $url["pass"];
+$dbname = ltrim($url["path"], '/');
+
+$dsn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+
+//$dsn = "pgsql:host=$host;port=5432;dbname=$db";
 
 try {
     $pdo = new PDO($dsn, $user, $pass);
